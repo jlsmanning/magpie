6a1bddfb22b18e4dd237e20127d47a8cdcb5aff9
Justine Manning <justinelouisemanning@gmail.com>
2026-01-20 15:20:30 -0500

    fixed gitignore, replace accidentally removed file

---
+++ magpie/core/query_processor.py
@@ -0,0 +1,8 @@
+# In query_processor.py when we create it
+class QueryProcessor:
+    """
+    Converts queries to embeddings and expands with related terms.
+    
+    TODO: Add support for boolean query operators (AND/OR/NOT)
+          for more precise retrieval when semantic search isn't enough.
+    """
